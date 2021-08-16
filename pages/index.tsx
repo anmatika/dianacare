b63df1d257a9332dc132e79bc9e8108d01bedca9
anmatika <4ntti.matikainen@gmail.com>
2021-08-16 10:53:03 +0300

    Use prose in the main page also

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,8 @@ function Home(props: any) {
 
   return (
     <>
-      <div className="flex justify-center p-32 pt-8">
+      <div className="prose p-8">
+        <h1>Diana cares</h1>
         <div>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
