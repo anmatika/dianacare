f69759259a9bf4eece82673644be27294d313a0f
anmatika <4ntti.matikainen@gmail.com>
2021-08-16 09:18:01 +0300

    contact page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -11,18 +11,34 @@ const containerStyle = {
 
 function Contact(props: any) {
   return (
-    <div>
-      <Map
-        google={props.google}
-        containerStyle={containerStyle}
-        initialCenter={{
-          lat: 61.49911,
-          lng: 23.78712
-        }}
-      >
-        <Marker
-        />
-      </Map></div>
+    <>
+      <div className="prose p-8"><h1>Löydät meidät täältä</h1>
+        <p className="mb-8">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+        <div>
+          <i className="fas fa-map-marker-alt mr-1"></i>
+          Itsenäisyydenkatu 1, 33100 Tampere
+        </div>
+        <div>
+          <i className="fas fa-phone-square mr-1"></i>
+          +358 123456
+        </div>
+
+      </div>
+      <div className="p-8">
+        <Map
+          google={props.google}
+          containerStyle={containerStyle}
+          initialCenter={{
+            lat: 61.49911,
+            lng: 23.78712
+          }}
+        >
+          <Marker
+          />
+        </Map></div>
+    </>
   )
 }
 console.log('google api key', mapsKey)
